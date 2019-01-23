d8c03d5d2a0841a17d27ac19a2c128841ec54f09
farhad <f.noori.niaraki@outlook.com>
2019-01-22 21:37:41 -0800

    rename some path map

--- src/main/java/com/tradeshift/companystructure/constants/CompanyNodePathMap.java
+++ src/main/java/com/tradeshift/companystructure/constants/CompanyNodePathMap.java
@@ -3,6 +3,6 @@ package com.tradeshift.companystructure.constants;
 public class CompanyNodePathMap {
 
     public static final String API_V1 = "/api/v1";
-    public static final String GET_ALL_CHILDREN_OF_GIVEN_NODE = "/companynodes/{id}/children";
-    public static final String CHANGE_PARENT_NODE_OF_GIVEN_NODE = "/companynodes/{id}/parent/{parentId}";
+    public static final String COMPANYNODES_ID_CHILDREN = "/companynodes/{id}/children";
+    public static final String COMPANYNODES_ID_PARENT_PARENTID = "/companynodes/{id}/parent/{parentId}";
 }
