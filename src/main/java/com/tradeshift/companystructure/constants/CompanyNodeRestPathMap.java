285137649524bab03ce56adc2a8435aa0f512c13
farhad <f.noori.niaraki@outlook.com>
2019-01-22 18:59:15 -0800

    add CompanyNodeRestApiPathMap

---
+++ src/main/java/com/tradeshift/companystructure/constants/CompanyNodeRestPathMap.java
@@ -0,0 +1,8 @@
+package com.tradeshift.companystructure.constants;
+
+public class CompanyNodeRestPathMap {
+
+    public static final String API_V1 = "/api/v1";
+    public static final String GET_ALL_CHILDREN_OF_GIVEN_NODE = "/companynodes/{id}/children";
+    public static final String CHANGE_PARENT_NODE_OF_GIVEN_NODE = "/companynodes/{id}/parent/{parentId}";
+}
