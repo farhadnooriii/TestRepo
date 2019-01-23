b6c6695238d71b71c0a780b47e1f5e957bb528c9
farhad <f.noori.niaraki@outlook.com>
2019-01-22 19:04:58 -0800

    add handleMethodDatabaseAccessFailure method

--- src/main/java/com/tradeshift/companystructure/controllers/RestApiExceptionHandlerController.java
+++ src/main/java/com/tradeshift/companystructure/controllers/RestApiExceptionHandlerController.java
@@ -1,45 +1,102 @@
 package com.tradeshift.companystructure.controllers;
 
+import com.tradeshift.companystructure.repositories.exceptions.EntityNotFoundException;
 import com.tradeshift.companystructure.viewmodels.error.RestApiErrorVM;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 /**
  * Created by F.Nouri on 1/22/2019.
  */
-@Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
-@Slf4j
 public class RestApiExceptionHandlerController extends ResponseEntityExceptionHandler {
 
     /**
-     * Handle MissingServletRequestParameterException. Triggered when a 'required' request parameter is missing.
+     * Convert restApiErrorVM to ResponseEntity.
      *
-     * @param ex      MissingServletRequestParameterException
-     * @param headers HttpHeaders
-     * @param status  HttpStatus
-     * @param request WebRequest
-     * @return the ApiError object
+     * @param restApiErrorVM the EntityNotFoundException
+     * @return the ResponseEntity<RestApiErrorVM>
      */
-    @Override
-    protected ResponseEntity<Object> handleMissingServletRequestParameter(
-            MissingServletRequestParameterException ex, HttpHeaders headers,
-            HttpStatus status, WebRequest request) {
-        String error = ex.getParameterName() + " parameter is missing";
-        return buildResponseEntity(new RestApiErrorVM(BAD_REQUEST, error, ex));
+    private ResponseEntity<Object> buildResponseEntity(RestApiErrorVM restApiErrorVM) {
+        return new ResponseEntity<>(restApiErrorVM, restApiErrorVM.getHttpStatus());
     }
 
-    private ResponseEntity<Object> buildResponseEntity(RestApiErrorVM restApiErrorVM) {
-        return new ResponseEntity<>(restApiErrorVM, restApiErrorVM.getStatus());
+    /**
+     * Handles When entity not found.
+     *
+     * @param ex the EntityNotFoundException
+     * @return the RestApiErrorVM object
+     */
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFound(EntityNotFoundException ex) {
+        RestApiErrorVM restApiErrorVM = new RestApiErrorVM(HttpStatus.NOT_FOUND);
+        restApiErrorVM.setMessage(ex.getMessage());
+        return buildResponseEntity(restApiErrorVM);
+    }
+
+    /**
+     * Handle Exception, handle generic Exception.class
+     *
+     * @param ex the Exception
+     * @return the RestApiErrorVM object
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        RestApiErrorVM restApiErrorVM = new RestApiErrorVM(HttpStatus.BAD_REQUEST);
+        restApiErrorVM.setMessage(String.format("The parameter '%s' of value '%s' could not be converted to type '%s'", ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName()));
+        restApiErrorVM.setDebugMessage(ex.getMessage());
+        restApiErrorVM.setExceptionClass(ex.getClass().getName());
+        return buildResponseEntity(restApiErrorVM);
+    }
+
+    /**
+     * Handle Exception, handle ResourceNotFoundException.class
+     *
+     * @param ex the Exception
+     * @return the RestApiErrorVM object
+     */
+    @ExceptionHandler(ResourceNotFoundException.class)
+    protected ResponseEntity<Object> handleMethodResourceNotFound(ResourceNotFoundException ex) {
+        RestApiErrorVM restApiErrorVM = new RestApiErrorVM(HttpStatus.NOT_FOUND);
+        restApiErrorVM.setMessage(String.format("The path not found"));
+        restApiErrorVM.setDebugMessage(ex.getMessage());
+        restApiErrorVM.setExceptionClass(ex.getClass().getName());
+        return buildResponseEntity(restApiErrorVM);
+    }
+
+    /**
+     * Handle DataAccessResourceFailureException.class exception.
+     *
+     * @param ex the Exception
+     * @return the RestApiErrorVM object
+     */
+    @ExceptionHandler(DataAccessResourceFailureException.class)
+    protected ResponseEntity<Object> handleMethodDatabaseAccessFailure(DataAccessResourceFailureException ex) {
+        RestApiErrorVM restApiErrorVM = new RestApiErrorVM(HttpStatus.BAD_REQUEST);
+        restApiErrorVM.setMessage(String.format("Database connection failed"));
+        restApiErrorVM.setDebugMessage(ex.getMessage());
+        restApiErrorVM.setExceptionClass(ex.getClass().getName());
+        return buildResponseEntity(restApiErrorVM);
+    }
+
+    /**
+     * Handle Exception.class exception.
+     *
+     * @param ex the Exception
+     * @return the RestApiErrorVM object
+     */
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<Object> handleDefaultMethod(Exception ex) {
+        RestApiErrorVM restApiErrorVM = new RestApiErrorVM(HttpStatus.BAD_REQUEST);
+        restApiErrorVM.setMessage(String.format("There is Exception. Contact to Administrator"));
+        restApiErrorVM.setDebugMessage(ex.getMessage());
+        restApiErrorVM.setExceptionClass(ex.getClass().getName());
+        return buildResponseEntity(restApiErrorVM);
     }
 }
