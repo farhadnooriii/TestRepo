fa468e4e5a2f75167c55a6b26f0dcada5c756b67
farhad <f.noori.niaraki@outlook.com>
2019-01-23 12:32:19 -0800

    add RES_COMPANYNODES_ID_CHILDREN and SELF key value

--- src/main/java/com/tradeshift/companystructure/constants/CompanyNodePathMap.java
+++ src/main/java/com/tradeshift/companystructure/constants/CompanyNodePathMap.java
@@ -4,5 +4,7 @@ public class CompanyNodePathMap {
 
     public static final String API_V1 = "/api/v1";
     public static final String COMPANYNODES_ID_CHILDREN = "/companynodes/{id}/children";
+    public static final String RES_COMPANYNODES_ID_CHILDREN = "/res/companynodes/{id}/children";
     public static final String COMPANYNODES_ID_PARENT_PARENTID = "/companynodes/{id}/parent/{parentId}";
+    public static final String SELF = "self";
 }
